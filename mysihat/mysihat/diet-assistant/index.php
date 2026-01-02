665e1f6b0b1ff85f96edc8c033f636b57decc36b
ruilinlee <liruilin610@gmail.com>
2026-01-02 19:03:51 +0800

    Update index.php

--- mysihat/mysihat/diet-assistant/index.php
+++ mysihat/mysihat/diet-assistant/index.php
@@ -194,7 +194,7 @@
         return String(s == null ? "" : s)
           .replace(/&/g, "&amp;")
           .replace(/</g, "&lt;")
-          .replace(/>/g, "&lt;")
+          .replace(/>/g, "&gt;")
           .replace(/"/g, "&quot;")
           .replace(/'/g, "&#039;");
       }
